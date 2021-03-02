6c2bb417fbcb28fc11c910eda41052d1114facbd
Demacio <Bobby.Demacio@ontario.ca>
2021-03-02 08:53:00 -0500

    comment pipeline test

--- src/main/java/ca/ontario/moh/hsc/prsb/reg/quarkusprj1/Animal.java
+++ src/main/java/ca/ontario/moh/hsc/prsb/reg/quarkusprj1/Animal.java
@@ -1,7 +1,7 @@
 package ca.ontario.moh.hsc.prsb.reg.quarkusprj1;
 
 public class Animal {
-    //Comment to test pipeline trigger
+    //Comment to test pipeline trigger 2
     String name;
     String color;
     String size;
