f3cdefc09a5946a38b5248976ccb7a9244370985
Demacio <Bobby.Demacio@ontario.ca>
2021-03-02 02:53:44 -0500

    added main class

---
+++ src/main/java/ca/ontario/moh/hsc/prsb/reg/quarkusprj1/Main.java
@@ -0,0 +1,13 @@
+package ca.ontario.moh.hsc.prsb.reg.quarkusprj1;
+
+import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.runtime.Quarkus;
+
+@QuarkusMain  
+public class Main {
+
+    public static void main(String ... args) {
+        System.out.println("Running main method");
+        Quarkus.run(args); 
+    }
+}
